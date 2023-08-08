7925e8c152ea7b3052699f1023d93e9396e3184a
romeupeniche <romeupeniche12@gmail.com>
2023-08-07 21:26:31 -0300

    env error fix

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,7 +3,9 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
+const firebaseConfig =
+  JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG) ??
+  JSON.parse(process.env.VITE_FIREBASE_CONFIG);
 
 const app = initializeApp(firebaseConfig);
 
